a253323a9dc8f0235a4ab3fb87c0885cef5334a7
alumichael <alumichael9@gmail.com>
2022-09-26 18:45:03 +0100

    Added Splash screen

--- app/src/main/java/com/dowell/dowellmap/activity/SplashActivity.kt
+++ app/src/main/java/com/dowell/dowellmap/activity/SplashActivity.kt
@@ -7,6 +7,8 @@ import android.os.Looper
 import androidx.lifecycle.Observer
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.lifecycleScope
+import androidx.viewbinding.BuildConfig
+import com.dowell.dowellmap.databinding.ActivitySplashScreenBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -16,14 +18,13 @@ class SplashActivity : BaseActivity() {
 
     private lateinit var binding: ActivitySplashScreenBinding
 
-    @Inject
-    lateinit var userpref: UserPref
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         with(binding){
+
             try {
                 val packageInfo = applicationContext.packageManager.getPackageInfo(
                     packageName, 0
@@ -34,34 +35,17 @@ class SplashActivity : BaseActivity() {
                 e.printStackTrace()
             }
 
-            userpref.isFirstLaunch().asLiveData().observe(this@SplashActivity, Observer
-            { hasloggedin->
-
-                if(hasloggedin!=null){
-
-                    Handler(Looper.getMainLooper()).postDelayed({
-                        if (hasloggedin) {
-                            startNewActivity(MainActivity::class.java)
-                        } else {
-                            lifecycleScope.launch {
-                                //userpref.setFirstLaunch(false)
-                                startNewActivity(FeatureActivity::class.java)
-                            }
-                        }
-                        try {
-                            this@SplashActivity.finishAffinity()
-                        } catch (e: NullPointerException) {
-                            if (BuildConfig.DEBUG) {
-                                e.printStackTrace()
-                            }
-                        }
-                    }, 5000)
-
-
+            Handler(Looper.getMainLooper()).postDelayed({
+                startNewActivity(MainActivity::class.java)
+                try {
+                    this@SplashActivity.finishAffinity()
+                } catch (e: NullPointerException) {
+                    if (BuildConfig.DEBUG) {
+                        e.printStackTrace()
+                    }
                 }
+            }, 5000)
 
-
-            })
 
         }
     }
